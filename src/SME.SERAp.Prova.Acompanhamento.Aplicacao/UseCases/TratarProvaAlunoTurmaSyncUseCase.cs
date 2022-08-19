9758ffdad7c599c6f921f353e2e8fbfbd93369a3
Juliano Martins <juliano_campos_92@hotmail.com>
2022-08-19 15:13:01 -0400

    sincronizar dados de uma prova

---
+++ src/SME.SERAp.Prova.Acompanhamento.Aplicacao/UseCases/TratarProvaAlunoTurmaSyncUseCase.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using SME.SERAp.Prova.Acompanhamento.Aplicacao.Interfaces;
+using SME.SERAp.Prova.Acompanhamento.Infra.Fila;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SME.SERAp.Prova.Acompanhamento.Aplicacao.UseCases
+{
+    public class TratarProvaAlunoTurmaSyncUseCase : AbstractUseCase, ITratarProvaAlunoTurmaSyncUseCase
+    {
+        public TratarProvaAlunoTurmaSyncUseCase(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public async Task<bool> Executar(MensagemRabbit mensagemRabbit)
+        {
+            long provaId = int.Parse(mensagemRabbit.Mensagem.ToString());
+
+            var provasTurmas = await mediator.Send(new ObterProvasTurmasSerapQuery(provaId));
+            if (provasTurmas != null && provasTurmas.Any())
+            {
+                foreach (var provaTurma in provasTurmas)
+                {
+                    await mediator.Send(new PublicaFilaRabbitCommand(RotaRabbit.ProvaAlunoTratar, provaTurma));
+                }
+            }
+
+            return true;
+        }
+    }
+}
