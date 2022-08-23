2f04fcb270d0f47bd4bb57be185f964a08e39425
krisamcom <68442245+krisamcom@users.noreply.github.com>
2022-08-23 14:13:29 -0300

    bug: 72595 - ajuste tempo médio provas

---
+++ src/SME.SERAp.Prova.Acompanhamento.Aplicacao/Commands/ProvaTurmaResultado/Excluir/ExcluirProvaTurmaResultadoCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using SME.SERAp.Prova.Acompanhamento.Dominio.Entities;
+
+namespace SME.SERAp.Prova.Acompanhamento.Aplicacao
+{
+    public class ExcluirProvaTurmaResultadoCommand : IRequest<bool>
+    {
+        public ExcluirProvaTurmaResultadoCommand(ProvaTurmaResultado provaTurmaResultado)
+        {
+            ProvaTurmaResultado = provaTurmaResultado;
+        }
+
+        public ProvaTurmaResultado ProvaTurmaResultado { get; set; }
+    }
+}
