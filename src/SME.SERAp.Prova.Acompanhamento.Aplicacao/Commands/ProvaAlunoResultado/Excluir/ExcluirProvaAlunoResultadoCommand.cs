7f23155a8efbbb162c61e4445423a0db1b7b7cf8
Caique Siqueira <latorrecaique@gmail.com>
2022-09-22 13:37:41 -0300

    AB#74667 -  Excluir prova aluno resultado

---
+++ src/SME.SERAp.Prova.Acompanhamento.Aplicacao/Commands/ProvaAlunoResultado/Excluir/ExcluirProvaAlunoResultadoCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SERAp.Prova.Acompanhamento.Aplicacao
+{
+   public class ExcluirProvaAlunoResultadoCommand : IRequest<bool>
+    {
+        public ExcluirProvaAlunoResultadoCommand(string provaAlunoResultadoId)
+        {
+            ProvaAlunoResultadoId = provaAlunoResultadoId;
+        }
+
+        public string ProvaAlunoResultadoId { get; set; }
+    }
+}
