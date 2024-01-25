c060a33242fbabafdbb1e5bffcda8d7e3a8949c9
fernando.groeler <fernando.groeler@gmail.com>
2024-01-25 15:07:11 -0400

    Ajustado para atuaizar os dados do resultado da turma.

---
+++ src/SME.SERAp.Prova.Acompanhamento.Infra/Utils/UtilTempoMedio.cs
@@ -0,0 +1,13 @@
+﻿namespace SME.SERAp.Prova.Acompanhamento.Infra
+{
+    public static class UtilTempoMedio
+    {
+        public static long CalcularTempoMedioEmMinutos(int? tempoTotal, long totalFinalizados)
+        {
+            if (tempoTotal is null or 0) 
+                return 0;
+            
+            return ((int)tempoTotal / 60) / totalFinalizados;
+        }        
+    }
+}
